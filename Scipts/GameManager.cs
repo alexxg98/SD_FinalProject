5c0cf170a4f142c783df5dcf73bacd6847171bed
Linda Wong <43160578+lwong0515@users.noreply.github.com>
2021-11-29 14:20:18 -0500

    need to fix gameover panel

---
+++ Scipts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject GameOverMenu;
+    public static bool gameOver = false;
+    public static bool gameHalted = false;
+
+    public static void EndGame()
+    {
+        if (gameOver == false)
+        {
+            gameOver = true;
+            Debug.Log("Game Over");
+            Time.timeScale = 0f;
+            // GameOverMenu.SetActive(true);
+            gameHalted = true;
+        }
+    }
+
+}
---
+++ Scipts/TimeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeController : MonoBehaviour
+{
+    public int countDownTime;
+    public TextMeshProUGUI timerUI;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        StartCoroutine(countDownTimer());
+    }
+    IEnumerator countDownTimer()
+    {
+        while (countDownTime > 0)
+        {
+            timerUI.text = "Time: " + countDownTime.ToString();
+            yield return new WaitForSeconds(1f);
+            countDownTime--;
+        }
+        Debug.Log("Time: " + countDownTime.ToString());
+        timerUI.text = "Game Over!";
+        yield return new WaitForSeconds(1f);
+        if (countDownTime == 0)
+        {
+            GameManager.EndGame();
+        }
+     
+    }
+
+}
