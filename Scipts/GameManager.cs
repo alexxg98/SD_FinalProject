98b0351f707e8ba31ab77eecdbac93c60f59d172
Linda Wong <43160578+lwong0515@users.noreply.github.com>
2021-12-05 23:50:21 -0500

    fixed gamemanager

--- Scipts/GameManager.cs
+++ Scipts/GameManager.cs
@@ -1,24 +1,53 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject GameOverMenu;
+    public GameObject gameOverMenu;
     public static bool gameOver = false;
     public static bool gameHalted = false;
 
-    public static void EndGame()
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameHalted)
+            {
+                ReplayGame();
+            }
+            else
+            {
+                EndGame();
+            }
+        }
+    }
+
+    public void EndGame()
     {
         if (gameOver == false)
         {
             gameOver = true;
             Debug.Log("Game Over");
+
+            gameOverMenu.SetActive(true);
             Time.timeScale = 0f;
-            // GameOverMenu.SetActive(true);
             gameHalted = true;
         }
+        
     }
+
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+        Time.timeScale = 1f;
+        gameHalted = false;
+    }
+    
+ 
+    
 
 }
