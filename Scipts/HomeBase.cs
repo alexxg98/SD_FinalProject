5fb25924f653df218e7dcecf09a161dbd8c3f475
Linda Wong <43160578+lwong0515@users.noreply.github.com>
2021-12-09 14:58:33 -0500

    homebase health trigger conditions

---
+++ Scipts/HomeBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeBase : MonoBehaviour
+{
+    public static int hp = 30;
+    void Update()
+    {
+        CheckHealth();
+        Debug.Log("Health = " + hp);
+    }
+    public void CheckHealth()
+    {
+        if (hp <= 0)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+    
+}
