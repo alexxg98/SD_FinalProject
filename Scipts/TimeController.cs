7d8641194f69def8eae819b850155c6f42116dcf
Linda Wong <43160578+lwong0515@users.noreply.github.com>
2021-12-10 18:47:38 -0500

    spawn waves

--- Scipts/TimeController.cs
+++ Scipts/TimeController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -9,6 +9,10 @@ public class TimeController : MonoBehaviour
     // GameManager.gameMF gf = new GameManager.gameMF();
     public int countDownTime;
     public TextMeshProUGUI timerUI;
+    public int GetTime()
+    {
+        return countDownTime;
+    }
     // Start is called before the first frame update
     private void Start()
     {
@@ -18,18 +22,22 @@ public class TimeController : MonoBehaviour
     {
         while (countDownTime > 0)
         {
+            if (countDownTime == 30)
+            {
+                Debug.Log("Spawn!");
+                FindObjectOfType<Waves>().SpawnWave();
+            }
             timerUI.text = "Time: " + countDownTime.ToString();
             yield return new WaitForSeconds(1f);
             countDownTime--;
         }
         Debug.Log("Time: " + countDownTime.ToString());
         timerUI.text = "Game Over!";
-        yield return new WaitForSeconds(1f);
+        //yield return new WaitForSeconds(1f);
         if (countDownTime == 0)
         {
             FindObjectOfType<GameManager>().EndGame();
         }
-
+        
     }
-
 }
---
+++ Scipts/Waves.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waves : MonoBehaviour
+{
+    public GameObject[] AlienWaves;
+    public void SpawnWave()
+    {
+        AlienWaves[1].SetActive(true);
+    }
+
+
+}
