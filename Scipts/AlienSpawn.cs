22a17aa624b964aa952968819b89a0a1022345e9
Alexandria Guo <44476083+alexxg98@users.noreply.github.com>
2021-12-10 17:06:30 -0500

    Fix random spawn

--- Scipts/AlienSpawn.cs
+++ Scipts/AlienSpawn.cs
@@ -8,18 +8,25 @@ public class AlienSpawn : MonoBehaviour
     public GameObject alienPrefab;
     // Start is called before the first frame update
     void Start()
-    { 
-        randSpawn = Random.Range(5, 10);
-        //Debug.Log("Random Spawn Time: " + randSpawn);
+    {
+        //randSpawn = GetRandomNum();
+        // Debug.Log("Random Spawn Time: " + randSpawn);
         // Spawn first Alien after 6 secs, and every random secs afterwards
-        InvokeRepeating("Spawn", 6, randSpawn);
-
+        //InvokeRepeating("Spawn", 6, randSpawn);
+        Invoke("Spawn", GetRandomNum());
     }
 
     void Spawn()
     {
         FindObjectOfType<LaneActivate>().RandComp();
         Instantiate(alienPrefab, transform.position, Quaternion.identity);
-        
+        Invoke("Spawn", GetRandomNum());
+    }
+
+    int GetRandomNum()
+    {
+        randSpawn = Random.Range(5, 15);
+        Debug.Log("Random Spawn Time: " + randSpawn);
+        return randSpawn; 
     }
 }
